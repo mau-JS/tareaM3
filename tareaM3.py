cb54df2cad878789e85ce56d7b5b01083602e2e9
Mauricio Juárez <iconoclastsym@gmail.com>
2022-11-18 13:48:20 -0600

    Segunda version estable

--- tareaM3.py
+++ tareaM3.py
@@ -15,6 +15,8 @@ def agent_portrayal(agent):
         #Caso en el que sea semáforo
         if isinstance(agent,entornoAgent):
             portrayal["Color"] = "black"
+        if isinstance(agent,SemaforoAgent):
+            portrayal["Color"] = "yellow"
         return portrayal
 
 
@@ -26,11 +28,14 @@ class CarAgent1(mesa.Agent):
         super().__init__(unique_id, model)
         self.nombre = unique_id
     def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
         x,y = self.pos
         self.model.grid.move_agent(self, (x+1,y))
 
     def step(self):
-        print("Creando agente " + self.nombre)
+        #print("Creando agente " + self.nombre)
         self.move()
 
 class CarAgent2(mesa.Agent):
@@ -40,6 +45,19 @@ class CarAgent2(mesa.Agent):
     def move(self):
         x,y = self.pos
         self.model.grid.move_agent(self, (x,y-1))
+
+    def step(self):
+        self.move()
+class SemaforoAgent(mesa.Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.nombre = unique_id
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos, moore=True, include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
     def step(self):
         self.move()
 
@@ -55,7 +73,6 @@ class entornoAgent(mesa.Agent):
         self.model.grid.move_agent(self, new_position)
     def step(self):
         self.move()
-        print("Creando agente " + self.nombre)
 
 class CarModel(mesa.Model):
     def __init__(self, N,width, height):
@@ -68,7 +85,55 @@ class CarModel(mesa.Model):
             
             #Creando agentes para carro
         conteo = 0
+        
         classes = [CarAgent1,CarAgent2]
+
+        s1 = SemaforoAgent("S" + str(1),self)
+        self.schedule.add(s1)
+        x1 = 4
+        y1 = 5 
+        self.grid.place_agent(s1,(4,5))
+        s2 = SemaforoAgent("S" + str(2),self)
+        self.schedule.add(s2)
+        x2 = 4
+        y2 = 5 
+        self.grid.place_agent(s2,(5,6))
+
+        for i in range (5):
+            for j in range(5):
+                c = entornoAgent("E" + str(conteo), self)
+                self.schedule.add(c)
+                x = i
+                y = j
+                self.grid.place_agent(c, (x, y))
+                conteo += 1
+        
+        for i in range (5):
+            for j in range(6,11):
+                c = entornoAgent("E" + str(conteo), self)
+                self.schedule.add(c)
+                x = i
+                y = j
+                self.grid.place_agent(c, (x, y))
+                conteo += 1
+        
+        for i in range (6,11):
+            for j in range(5):
+                c = entornoAgent("E" + str(conteo), self)
+                self.schedule.add(c)
+                x = i
+                y = j
+                self.grid.place_agent(c, (x, y))
+                conteo += 1
+
+        for i in range (6,11):
+            for j in range(6,11):
+                c = entornoAgent("E" + str(conteo), self)
+                self.schedule.add(c)
+                x = i
+                y = j
+                self.grid.place_agent(c, (x, y))
+                conteo += 1
         for i in range(self.numAgentsCar):
             #instance = random.choice(classes)()
             a = random.choice(classes)("C" + str(i), self)
@@ -78,7 +143,6 @@ class CarModel(mesa.Model):
             if isinstance(a,CarAgent1):
                 x = 0
                 y = 5
-                
                 self.grid.place_agent(a,(x,y))
 
             if isinstance(a,CarAgent2):
@@ -86,41 +150,7 @@ class CarModel(mesa.Model):
                 y = 10
                 self.grid.place_agent(a,(x,y))
         
-        for i in range (5):
-            for j in range(5):
-                c = entornoAgent("E" + str(conteo), self)
-                self.schedule.add(c)
-                x = i
-                y = j
-                self.grid.place_agent(c, (x, y))
-                conteo += 1
-        
-        for i in range (5):
-            for j in range(6,11):
-                c = entornoAgent("E" + str(conteo), self)
-                self.schedule.add(c)
-                x = i
-                y = j
-                self.grid.place_agent(c, (x, y))
-                conteo += 1
-        
-        for i in range (6,11):
-            for j in range(5):
-                c = entornoAgent("E" + str(conteo), self)
-                self.schedule.add(c)
-                x = i
-                y = j
-                self.grid.place_agent(c, (x, y))
-                conteo += 1
 
-        for i in range (6,11):
-            for j in range(6,11):
-                c = entornoAgent("E" + str(conteo), self)
-                self.schedule.add(c)
-                x = i
-                y = j
-                self.grid.place_agent(c, (x, y))
-                conteo += 1
 
 
         
@@ -137,7 +167,7 @@ class CarModel(mesa.Model):
 
 grid = mesa.visualization.CanvasGrid(agent_portrayal, 11, 11, 500, 500)
 server = mesa.visualization.ModularServer(
-    CarModel, [grid], "Car Model", {"N": 2, "width": 11, "height": 11}
+    CarModel, [grid], "Car Model", {"N": 1, "width": 11, "height": 11}
 )
 
 server.port = 8521 # The default
